8501686ac3a1a58db1f655739a948c1c1a07c0df
m2k-dcyh13 <219132320+m2k-dcyh13@users.noreply.github.com>
2026-03-04 16:49:58 +0900

    초기화(app, db)

--- backend/common/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Code(models.Model):
-    group = models.CharField(max_length=50)
-    code = models.CharField(max_length=50)
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
--- backend/maps/models.py
+++
@@ -1,6 +0,0 @@
-# map/models.py
-from django.db import models
-
-class Map(models.Model):
-    latitude = models.FloatField()
-    longitude = models.FloatField()
