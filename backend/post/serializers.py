3902403a62faffb0a17557a47e1eceb7f3e577c9
m2k-dcyh13 <219132320+m2k-dcyh13@users.noreply.github.com>
2026-03-18 18:58:34 +0900

    feature/rest_api post serializers 500error 수정 2차

--- backend/post/serializers.py
+++ backend/post/serializers.py
@@ -45,10 +45,9 @@ class PostBaseSerializer(serializers.ModelSerializer):
 
     def get_map(self, obj):
         try:
-            # map_obj = Maps.objects.get(post=obj) 
-            # return MapBaseSerializer(map_obj).data
-            return MapBaseSerializer(obj.map).data  # post→map 방향
-        except Exception: # Maps.DoesNotExist
+
+            return MapBaseSerializer(obj.map).data 
+        except Exception:
             return None
 
 '''==============================================================='''
@@ -87,10 +86,11 @@ class PostCreateSerializer(PostBaseSerializer):
 
         latitude = validated_data.pop('latitude')
         longitude = validated_data.pop('longitude')
-        address_cd = validated_data.pop('address_cd')
+        address_cd = validated_data.pop('address_cd', None)
         address_detail = validated_data.pop('address_detail')
         image_url = validated_data.pop('image_url', None)
         post_cd_val = validated_data.pop('post_cd', None)
+        status_cd = validated_data.pop('status_cd', None)
 
 
         address_code_instance = CodeT.objects.filter(cd = address_cd).first()
@@ -104,6 +104,7 @@ class PostCreateSerializer(PostBaseSerializer):
         post = Posts.objects.create(
             map=new_map,
             post_cd = CodeT.objects.filter(cd=post_cd_val).first() if post_cd_val else None,
+            status_cd = CodeT.objects.filter(cd=status_cd).first() if status_cd else None
             **validated_data
         )
 
