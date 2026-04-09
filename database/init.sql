47e63195c103bc56d2bdd90ea251e0529bfc60a2
m2k-dcyh13 <219132320+m2k-dcyh13@users.noreply.github.com>
2026-04-09 10:05:13 +0900

    4/9 init.sql

--- database/init.sql
+++ database/init.sql
@@ -1,20 +1,23 @@
-CREATE TABLE codeT(
-    cd VARCHAR(6) PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
-    cd_info TEXT NOT NULL,
-    cd_upper VARCHAR(6) NULL,
+--코드테이블--
+CREATE TABLE codeT( 
+    cd VARCHAR(6) PRIMARY KEY, --코드--
+    name VARCHAR(30) NOT NULL, --이름--
+    cd_info TEXT NOT NULL, --코드설명--
+    cd_upper VARCHAR(6) NULL, --부모코드--
 );
 
+--지도테이블--
 CREATE TABLE maps(
-    map_id BIGSERIAL PRIMARY KEY,
+    map_id BIGSERIAL PRIMARY KEY, 
     name TEXT,
     category_cd VARCHAR(6) NOT NULL REFERENCES codeT(cd),
-    address_cd VARCHAR(6) NOT NULL REFERENCES codeT(cd),
+    address_cd VARCHAR(6) NOT NULL REFERENCES codeT(cd), --주소 대분류(시군구 단위)--
     address_detail TEXT NOT NULL,
     latitude FLOAT NOT NULL,
     longitude FLOAT NOT NULL,
 );
 
+--가게(맛집,...) 테이블
 CREATE TABLE shop(
     shop_id BIGSERIAL PRIMARY KEY,
     map_id BIGINT NOT NULL REFERENCES maps(map_id),
@@ -22,7 +25,7 @@ CREATE TABLE shop(
     rating FLOAT
 );
 
-
+--포스트 테이블 (맛집 댓글, 크롤링 게시글, 커뮤니티, 게시글)--
 CREATE TABLE posts(
     post_id BIGSERIAL PRIMARY KEY,
     title varchar(100) NOT NULL,
@@ -30,9 +33,9 @@ CREATE TABLE posts(
     created_at TIMESTAMP NOT NULL,
     modify_at TIMESTAMP NOT NULL,
     status_cd VARCHAR(6) NOT NULL REFERENCES codeT(cd),
-    post_cd VARCHAR(6) NOT NULL REFERENCES codeT(cd),
+    post_cd VARCHAR(6) NOT NULL REFERENCES codeT(cd), --post_cd를 통해 게시글과 커뮤니티, 댓글을 구분한다)--
     --user_id BIGINT NOT NULL REFERENCES users(user_id),--
-    map_id BIGINT REFERENCES maps(map_id),
+    map_id BIGINT REFERENCES maps(map_id), --만약 커뮤니티나 맛집에 대한 내용이라면 map_id 작성--
     shop_id BIGINT REFERENCES shop(shop_id),
 
 );
