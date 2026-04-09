c0ef16c081a3d3be52dce5b64b81e99facb96c21
m2k-dcyh13 <219132320+m2k-dcyh13@users.noreply.github.com>
2026-04-09 17:25:41 +0900

    db 완료

--- backend/foodm/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import FoodMapView, FoodMapDetailView
-
-urlpatterns = [
-    # 전체 조회 - GET /api/foodmap/
-    path("", FoodMapView.as_view()),
-    # 특정 맛집 조회 - GET /api/foodmap/{map_id}/
-    path("<int:map_id>/", FoodMapDetailView.as_view()),
-]
--- database/erd.md
+++ database/erd.md
@@ -1,2 +1,3 @@
 <!-- erd 링크 정보 -->
 
+![ERD](../doc/SKN-Toy_2차.png)
