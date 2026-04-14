891bd1d8cbc314ab41403a307470993c7f5d8c7e
m2k-dcyh13 <219132320+m2k-dcyh13@users.noreply.github.com>
2026-04-14 21:02:04 +0900

    db 컬럼 정리 완료

--- database/init.sql
+++ database/init.sql
@@ -36,20 +36,6 @@ CREATE TABLE "shop"(
     rating FLOAT --평점--
 );
 
---포스트 테이블 (맛집 댓글, 크롤링 게시글, 커뮤니티, 게시글)--
-CREATE TABLE "posts"(
-    post_id BIGSERIAL PRIMARY KEY, --게시글 고유 번호--
-    title varchar(100) NOT NULL, --게시글 제목--
-    content TEXT NOT NULL, --게시글 내용--
-    created_at TIMESTAMP NOT NULL, --게시글 생성일--
-    modify_at TIMESTAMP NOT NULL, --게시글 수정일--
-    status_cd VARCHAR(6) NOT NULL REFERENCES "codeT"(cd), --게시글 상태(정상/삭제)--
-    post_cd VARCHAR(6) NOT NULL REFERENCES "codeT"(cd), --post_cd를 통해 게시글과 커뮤니티, 댓글을 구분한다)--
-    user_id BIGINT REFERENCES users(user_id),
-    map_id BIGINT REFERENCES maps(map_id) ON DELETE SET NULL, --만약 커뮤니티나 맛집에 대한 내용이라면 map_id 작성--
-    shop_id BIGINT REFERENCES shop(shop_id) ON DELETE SET NULL, --만약 맛집에 대한 내용이라면 shop_id 작성--
-    crawling_id BIGINT REFERENCES crawling(crawling_id) ON DELETE SET NULL --크롤링 고유 번호-- (FK는 아래에서 추가)
-);
 
 CREATE TABLE "crawling"(
     crawling_id BIGSERIAL PRIMARY KEY, --크롤링 고유 번호--
@@ -64,6 +50,21 @@ CREATE TABLE "crawling"(
     author VARCHAR(100), --작성자--
     map_id BIGINT REFERENCES maps(map_id) ON DELETE SET NULL, --지도 고유 번호--
     category_cd VARCHAR(6) REFERENCES "codeT"(cd) --it/정보--
+);
+
+--포스트 테이블 (맛집 댓글, 크롤링 게시글, 커뮤니티, 게시글)--
+CREATE TABLE "posts"(
+    post_id BIGSERIAL PRIMARY KEY, --게시글 고유 번호--
+    title varchar(100) NOT NULL, --게시글 제목--
+    content TEXT NOT NULL, --게시글 내용--
+    created_at TIMESTAMP NOT NULL, --게시글 생성일--
+    modify_at TIMESTAMP NOT NULL, --게시글 수정일--
+    status_cd VARCHAR(6) NOT NULL REFERENCES "codeT"(cd), --게시글 상태(정상/삭제)--
+    post_cd VARCHAR(6) NOT NULL REFERENCES "codeT"(cd), --post_cd를 통해 게시글과 커뮤니티, 댓글을 구분한다)--
+    user_id BIGINT REFERENCES users(user_id),
+    map_id BIGINT REFERENCES maps(map_id) ON DELETE SET NULL, --만약 커뮤니티나 맛집에 대한 내용이라면 map_id 작성--
+    shop_id BIGINT REFERENCES shop(shop_id) ON DELETE SET NULL, --만약 맛집에 대한 내용이라면 shop_id 작성--
+    crawling_id BIGINT REFERENCES crawling(crawling_id) ON DELETE SET NULL --크롤링 고유 번호-- (FK는 아래에서 추가)
 );
 
 
