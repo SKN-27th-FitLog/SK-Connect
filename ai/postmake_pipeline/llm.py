bb2c2a955b8bdd4099b2cff9f196c79d43dcfdf2
m2k-dcyh13 <219132320+m2k-dcyh13@users.noreply.github.com>
2026-04-23 00:26:54 +0900

    prompt 수정

--- ai/postmake_pipeline/llm.py
+++ ai/postmake_pipeline/llm.py
@@ -1,13 +1,33 @@
 from langchain_ollama import ChatOllama
+from typing import TypedDict, List
+from llm import get_llm
+from prompt import Prompt
 
-model = ChatOllama(
-    model="gemma4:e4b",
-    temperature=0.1,
-    top_p=1.0,
-    num_predict=500,
-    keep_alive="20m"
-)
+def get_llm():
+    return ChatOllama(
+        model="gemma4:e4b",
+        temperature=0.1,
+        top_p=1.0,
+        num_predict=500,
+        keep_alive="20m"
+    )
+
 # 게시글 생성
 #gemma4:e4b
 
 #embedding
+
+#similarity_search
+
+#conditional_edge
+
+class State(TypedDict):
+    data: dict
+    post: str
+    embedding: List[float]
+    is_unique: bool
+
+def generate_node():
+    llm = get_llm()
+    prompt = Prompt()
+    return llm.invoke(prompt)
