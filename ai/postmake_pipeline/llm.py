0fdfe3017fa7fd94d5c5e3ea44047cd2eb853950
m2k-dcyh13 <219132320+m2k-dcyh13@users.noreply.github.com>
2026-04-22 21:27:05 +0900

    prompt

--- ai/postmake_pipeline/llm.py
+++ ai/postmake_pipeline/llm.py
@@ -1,3 +1,13 @@
+from langchain_ollama import ChatOllama
+
+model = ChatOllama(
+    model="gemma4:e4b",
+    temperature=0.1,
+    top_p=1.0,
+    num_predict=500,
+    keep_alive="20m"
+)
 # 게시글 생성
+#gemma4:e4b
 
 #embedding
